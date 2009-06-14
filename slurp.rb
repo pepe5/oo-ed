90f3c67e1ec6e873ae4287855bfad6bd672aaa5f
pep <pep@pb-laptop.(none)>
2009-06-14 22:06:31 +0200

    branches tips

--- slurp.rb
+++ slurp.rb
@@ -1,4 +1,6 @@
-# eat file-based commands
+## eat file-based commands
+# @TODO try j ruby server
+# @TODO try ruby message bus (branch)
 puts "slurp starts.. @ "+Time.now.localtime.strftime("%Y-%m-%d*%H:%M:%S")
 i = 3
 while (i-=1)>0 do # true
