0e0e6e383d749ead9d41931b34fc5cbf7f9d1329
pep <pep@pb-laptop.(none)>
2009-06-14 21:56:56 +0200

    slurp starting lines (from pep account)

--- slurp.rb
+++ slurp.rb
@@ -1,2 +1,9 @@
 # eat file-based commands
-require Logger
+puts "slurp starts.. @ "+Time.now.localtime.strftime("%Y-%m-%d*%H:%M:%S")
+i = 3
+while (i-=1)>0 do # true
+  # (d = Dir .new '.') .entries
+  # `ls -alF`
+  puts "i: #{i}"
+  sleep 1
+end
